70191e765abd9d4e31cf571a8839f0b689d0b8ae
Ben Fradet <benjamin.fradet@gmail.com>
2018-11-30 11:16:48 +0000

    Remove unlawful monoid instance for RecoveryScenario

--- core/src/main/scala/com.snowplowanalytics.snowplow.event.recovery/RecoveryScenario.scala
+++ core/src/main/scala/com.snowplowanalytics.snowplow.event.recovery/RecoveryScenario.scala
@@ -1,8 +1,6 @@
 package com.snowplowanalytics.snowplow
 package event.recovery
 
-import cats.Monoid
-
 import CollectorPayload.thrift.model1.CollectorPayload
 import model._
 
@@ -14,21 +12,6 @@ sealed trait RecoveryScenario {
 }
 
 object RecoveryScenario {
-  implicit val monoid: Monoid[RecoveryScenario] = new Monoid[RecoveryScenario] {
-    def combine(r1: RecoveryScenario, r2: RecoveryScenario): RecoveryScenario =
-      new RecoveryScenario {
-        def error: String = r1.error + r2.error
-        override def filter(errors: List[Error]): Boolean = r1.filter(errors) || r2.filter(errors)
-        def mutate(originalPayload: CollectorPayload): CollectorPayload =
-          (r1.mutate _ andThen r2.mutate)(originalPayload)
-      }
-
-    def empty: RecoveryScenario = new RecoveryScenario {
-      def error: String = ""
-      override def filter(errors: List[Error]): Boolean = false
-      def mutate(originalPayload: CollectorPayload): CollectorPayload = originalPayload
-    }
-  }
 
   final case class ReplaceInQueryString(
     error: String,
