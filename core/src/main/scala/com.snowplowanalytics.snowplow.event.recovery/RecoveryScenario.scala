f34a2fbc118d82c029f3d74a0f05cb32ffc572af
Ben Fradet <benjamin.fradet@gmail.com>
2018-12-28 16:37:43 +0100

    Add scaladoc (closes #10)

--- core/src/main/scala/com.snowplowanalytics.snowplow.event.recovery/RecoveryScenario.scala
+++ core/src/main/scala/com.snowplowanalytics.snowplow.event.recovery/RecoveryScenario.scala
@@ -29,22 +29,53 @@ import io.lemonlabs.uri.QueryString
 import CollectorPayload.thrift.model1.CollectorPayload
 import model._
 
+/**
+ * Trait common to all recovery scenarios which, in essence, contains two things:
+ * - a way to find out if a recovery scenario should be applied to a bad row, the error filter
+ * - a way to fix the collector payload contained in the bad row, the mutation function
+ */
 sealed trait RecoveryScenario {
+  /** Error discriminant used to check if a recovery scenario should be applied to a bad row. */
   def error: String
+
+  /**
+   * Function used to check if a recovery scenario should be applied to a bad row given its errors.
+   * @param errors list of errors contained in a [[BadRow]]
+   * @return true if there exists at least one error in the provided bad row errors which contains
+   * this [[RecoveryScenario]]'s error
+   */
   def filter(errors: List[model.Error]): Boolean =
     errors.map(_.message).exists(_.contains(error))
+
+  /**
+   * Function mutating a [[CollectorPayload]].
+   * @param originalPayload [[CollectorPayload]] before mutation
+   * @return a fixed [[CollectorPayload]]
+   */
   def mutate(originalPayload: CollectorPayload): CollectorPayload
 }
 
 object RecoveryScenario {
 
-  // Querystring recovery scenarios
+  // Query string recovery scenarios
 
+  /**
+   * Recovery scenario replacing part of a collector payload's query string.
+   * @param error discriminant used to check if a recovery scenario should be applied to a bad row
+   * @param toReplace part of the query string that needs replacing
+   * @param replacement for the part of the query string that needs to be replaced
+   */
   final case class ReplaceInQueryString(
     error: String,
     toReplace: String,
     replacement: String
   ) extends RecoveryScenario {
+    /**
+     * Mutate a collector payload by replacing part of its query string.
+     * @param originalPayload the payload before applying the recovery scenario
+     * @return a collector payload with part of its query string replaced, the payload remains
+     * unchanged if the it doesn't have a query string
+     */
     def mutate(originalPayload: CollectorPayload): CollectorPayload = (for {
       qs <- Option(originalPayload.querystring)
       replaced <- replaceAll(qs, toReplace, replacement)
@@ -54,12 +85,27 @@ object RecoveryScenario {
     }).getOrElse(originalPayload)
   }
 
+  /**
+   * Recovery scenario replacing part of a collector payload's base64-encoded field in its query
+   * string.
+   * @param error discriminant used to check if a recovery scenario should be applied to a bad row
+   * @param base64Field base64-encoded field in the query string, ue_px or cx
+   * @param toReplace part of the base64-encoded field in the query string that needs replacing
+   * @param replacement for the part of the base64-encoded field in the query string that needs
+   * to be replaced
+   */
   final case class ReplaceInBase64FieldInQueryString(
     error: String,
     base64Field: String,
     toReplace: String,
     replacement: String
   ) extends RecoveryScenario {
+    /**
+     * Mutate a collector payload by replacing part of a base64-encoded field in its query string.
+     * @param originalPayload the payload before applying the recovery scenario
+     * @return a collector payload with part of a base64-encoded field in its query string replaced,
+     * the payload remains unchanged if the it doesn't have a query string
+     */
     def mutate(originalPayload: CollectorPayload): CollectorPayload = (for {
       rawQs <- Option(originalPayload.querystring)
       qs <- QueryString.parseOption(rawQs)
@@ -73,7 +119,18 @@ object RecoveryScenario {
     }).getOrElse(originalPayload)
   }
 
+  /**
+   * Recovery scenario removing part of a collector payload's query string.
+   * @param error discriminant used to check if a recovery scenario should be applied to a bad row
+   * @param toRemove part of the query string that needs removing
+   */
   final case class RemoveFromQueryString(error: String, toRemove: String) extends RecoveryScenario {
+    /**
+     * Mutate a collector payload by removing part of its query string.
+     * @param originalPayload the payload before applying the recovery scenario
+     * @return a collector payload with part of its query string removed, the payload remains
+     * unchanged if it doesn't have a query string
+     */
     def mutate(originalPayload: CollectorPayload): CollectorPayload = (for {
       qs <- Option(originalPayload.querystring)
       removed <- replaceAll(qs, toRemove, "")
@@ -85,11 +142,23 @@ object RecoveryScenario {
 
   // Body recovery scenarios
 
+  /**
+   * Recovery scenario replacing part of a collector payload's body.
+   * @param error discriminant used to check if a recovery scenario should be applied to a bad row
+   * @param toReplace part of the body that needs replacing
+   * @param replacement for the part of the body that needs to be replaced
+   */
   final case class ReplaceInBody(
     error: String,
     toReplace: String,
     replacement: String
   ) extends RecoveryScenario {
+    /**
+     * Mutate a collector payload by replacing part of its body.
+     * @param originalPayload the payload before applying the recovery scenario
+     * @return a collector payload with part of its body replaced, the payload remains
+     * unchanged if the it doesn't have a body
+     */
     def mutate(originalPayload: CollectorPayload): CollectorPayload = (for {
       body <- Option(originalPayload.body)
       replaced <- replaceAll(body, toReplace, replacement)
@@ -103,12 +172,26 @@ object RecoveryScenario {
     root.data.each.ue_px.string.modify(f)
   private val modifyCx: (String => String) => (Json => Json) = (f: (String => String)) =>
     root.data.each.cx.string.modify(f)
+  /**
+   * Recovery scenario replacing part of a collector payload's base64-encoded field in its body.
+   * @param error discriminant used to check if a recovery scenario should be applied to a bad row
+   * @param base64Field base64-encoded field in the body, ue_px or cx
+   * @param toReplace part of the base64-encoded field in the body that needs replacing
+   * @param replacement for the part of the base64-encoded field in the body that needs to be
+   * replaced
+   */
   final case class ReplaceInBase64FieldInBody(
     error: String,
     base64Field: String,
     toReplace: String,
     replacement: String
   ) extends RecoveryScenario {
+    /**
+     * Mutate a collector payload by replacing part of a base64-encoded field in its body.
+     * @param originalPayload the payload before applying the recovery scenario
+     * @return a collector payload with part of a base64-encoded field in its body replaced, the
+     * payload remains unchanged if the it doesn't have a query string
+     */
     def mutate(originalPayload: CollectorPayload): CollectorPayload = (for {
       rawBody <- Option(originalPayload.body)
       body <- parse(rawBody).toOption
@@ -124,7 +207,18 @@ object RecoveryScenario {
     }).getOrElse(originalPayload)
   }
 
+  /**
+   * Recovery scenario removing part of a collector payload's body.
+   * @param error discriminant used to check if a recovery scenario should be applied to a bad row
+   * @param toRemove part of the body that needs removing
+   */
   final case class RemoveFromBody(error: String, toRemove: String) extends RecoveryScenario {
+    /**
+     * Mutate a collector payload by removing part of its body.
+     * @param originalPayload the payload before applying the recovery scenario
+     * @return a collector payload with part of its body removed, the payload remains unchanged if
+     * it doesn't have a body
+     */
     def mutate(originalPayload: CollectorPayload): CollectorPayload = (for {
       body <- Option(originalPayload.body)
       removed <- replaceAll(body, toRemove, "")
@@ -136,7 +230,16 @@ object RecoveryScenario {
 
   // Other recovery scenarios
 
+  /**
+   * Recovery scenario leaving the collector payload unchanged.
+   * @param error discriminant used to check if a recovery scenario should be applied to a bad row
+   */
   final case class PassThrough(error: String) extends RecoveryScenario {
+    /**
+     * Does not mutate collector payloads.
+     * @param originalPayload the payload before applying the recovery scenario
+     * @return the original payload unchanged
+     */
     def mutate(originalPayload: CollectorPayload): CollectorPayload = originalPayload
   }
 
