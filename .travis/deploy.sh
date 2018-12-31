6dc549ec5c6f02539eeab839c005c246d88d9ff2
Ben Fradet <benjamin.fradet@gmail.com>
2018-12-31 14:10:35 +0100

    Add CI/CD for the Beam job (closes #12)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,4 +5,5 @@ tag=$1
 cd $TRAVIS_BUILD_DIR
 
 export TRAVIS_BUILD_RELEASE_TAG=${tag}
-release-manager --config ./.travis/release.yml --check-version --make-artifact --upload-artifact
+release-manager --config ./.travis/release_spark.yml --check-version --make-artifact --upload-artifact
+release-manager --config ./.travis/release_beam.yml --check-version --make-artifact --upload-artifact
