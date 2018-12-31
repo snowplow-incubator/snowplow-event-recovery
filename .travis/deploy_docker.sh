0f53a80575853fd174b485b67a81fc4fb509e770
Ben Fradet <benjamin.fradet@gmail.com>
2018-12-31 17:13:26 +0100

    f ci/cd beam

--- .travis/deploy_docker.sh
+++ .travis/deploy_docker.sh
@@ -13,7 +13,8 @@ cd $TRAVIS_BUILD_DIR
 project_version=$(sbt version -Dsbt.log.noformat=true | perl -ne 'print "$1\n" if /info.*(\d+\.\d+\.\d+[^\r\n]*)/' | tail -n 1 | tr -d '\n')
 if [[ "${tag}" = *"${project_version}" ]]; then
     sbt "project beam" docker:publishLocal
-    docker push "snowplow-event-recovery-beam:${tag}" "${docker_repo}/snowplow/snowplow-event-recovery-beam:${tag}"
+    docker tag "snowplow-event-recovery-beam:${tag}" "${docker_repo}/snowplow/snowplow-event-recovery-beam:${tag}"
+    docker push "${docker_repo}/snowplow/snowplow-event-recovery-beam:${tag}"
 else
     echo "Tag version '${tag}' doesn't match version in scala project ('${project_version}'). Aborting!"
     exit 1
