40d04b1550ab9f7ec90e6a5ce248d68233dc4b2e
Piotr Limanowski <piotr@codearsonist.com>
2020-05-05 12:25:41 +0200

    [FIXUP] Deployment version check

--- .travis/deploy_docker.sh
+++ .travis/deploy_docker.sh
@@ -10,7 +10,7 @@ curl -X GET \
 
 cd $TRAVIS_BUILD_DIR
 
-project_version=$(sbt version -Dsbt.log.noformat=true | perl -ne 'print "$1\n" if /info.*(\d+\.\d+\.\d+[^\r\n]*)/' | tail -n 1 | tr -d '\n')
+project_version=$(sbt beam/version -Dsbt.log.noformat=true | perl -ne 'print "$1\n" if /info.*(\d+\.\d+\.\d+[^\r\n]*)/' | tail -n 1 | tr -d '\n')
 if [[ "${tag}" = *"${project_version}" ]]; then
     sbt "project beam" docker:publishLocal
     docker tag "snowplow-event-recovery-beam:${tag}" "${docker_repo}/snowplow/snowplow-event-recovery-beam:${tag}"
