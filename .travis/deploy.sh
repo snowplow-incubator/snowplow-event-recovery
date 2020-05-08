3db0232ec24c6a37920a86b3a432206ac45db373
Anton Parkhomenko <mailbox@chuwy.me>
2020-05-08 16:13:00 +0700

    Add Bintray credentials to .travis.yml (closes #56)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,3 +8,14 @@ export TRAVIS_BUILD_RELEASE_TAG=${tag}
 release-manager --config ./.travis/release_beam.yml --check-version --make-artifact --upload-artifact
 release-manager --config ./.travis/release_flink.yml --check-version --make-artifact --upload-artifact
 release-manager --config ./.travis/release_spark.yml --check-version --make-artifact --upload-artifact
+
+export BINTRAY_USER=${BINTRAY_SNOWPLOW_MAVEN_USER}
+export BINTRAY_PASS=${BINTRAY_SNOWPLOW_MAVEN_API_KEY}
+
+project_version=$(sbt version -Dsbt.log.noformat=true | perl -ne 'print "$1\n" if /info.*(\d+\.\d+\.\d+[^\r\n]*)/' | tail -n 1 | tr -d '\n')
+if [[ "${tag}" = *"${project_version}" ]]; then
+    sbt "project core" publish
+else
+    echo "Tag version '${tag}' doesn't match version in scala project ('${project_version}'). Aborting!"
+    exit 1
+fi
