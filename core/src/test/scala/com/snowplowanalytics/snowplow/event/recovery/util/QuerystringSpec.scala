0443383354b7bda60b8881fc7ec5962300a8cfc1
Piotr Limanowski <piotr@codearsonist.com>
2020-07-30 16:10:09 +0200

    Add RFC-based character-encoding to URL fixing (closes #66)
    This adds a more advanced approach to fixing characters in query
    parameters. It uses a parsec to analyze the structure of the string
    in the param. Each character or string subset is therefore either
    an reserved, unreserved, hex-encoded or invalid character.
    The characters that are invalid are then encoded and the new
    value is set.

--- core/src/test/scala/com/snowplowanalytics/snowplow/event/recovery/util/QuerystringSpec.scala
+++ core/src/test/scala/com/snowplowanalytics/snowplow/event/recovery/util/QuerystringSpec.scala
@@ -15,6 +15,9 @@
 package com.snowplowanalytics.snowplow
 package event.recovery
 
+import java.net.{URLDecoder, URLEncoder}
+import java.nio.charset.StandardCharsets.UTF_8
+import cats.syntax.either._
 import org.scalatest._
 import org.scalatest.Matchers._
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
@@ -28,24 +31,49 @@ class QuerystringSpec extends WordSpec with ScalaCheckPropertyChecks with Either
 
   "querystring" should {
     "extract params from string" in {
-
-      forAll(querystringGen) { qs =>
+      forAll(querystringGen(paramGen)) { qs =>
         val p = params(qs)
         p.right.value.size shouldEqual qs.split("&").size
       }
     }
-    "extract values from query params" in {
-      forAll(paramGen) { p =>
-        (clean(p)).r.findFirstIn(p).isDefined should be(true)
+    "fix known querystring issues" in {
+      val issues =
+        "e={ue}&tv=${js}&ue_px={{unknown}}&pv=[pv !@]&er=(aaa,bbb)&zx=iglu:test&xz='lorem'"
+      val expected = Map(
+        "e"     -> "%7Bue%7D",
+        "tv"    -> "$%7Bjs%7D",
+        "pv"    -> "[pv+!@]",
+        "ue_px" -> "%7B%7Bunknown%7D%7D",
+        "er"    -> "(aaa,bbb)",
+        "zx"    -> "iglu:test",
+        "xz"    -> "'lorem'"
+      )
+      val recovered = params(issues).map(_.mapValues(clean)).right.value
+      recovered shouldEqual expected
+      mkQS(recovered) shouldEqual "e=%7Bue%7D&zx=iglu:test&tv=$%7Bjs%7D&pv=[pv+!@]&xz='lorem'&ue_px=%7B%7Bunknown%7D%7D&er=(aaa,bbb)"
+      javaDecode(mkQS(recovered)) should be('right)
+    }
+    "not manipulate iglu uris" in {
+      val uri = "iglu:com.snowplowanalytics.snowplow/recoveries/jsonschema/4-0-0"
+      clean(uri) shouldEqual uri
+      javaDecode(uri) should be('right)
+    }
+    "fix special characters" in {
+      forAll { (p: String) =>
+        javaEncode(p).right.value shouldEqual clean(p)
       }
     }
     "convert strings to lists of NVPs and back" in {
-      forAll(querystringGen) { qs =>
-        fromNVP(toNVP(clean(qs))) shouldEqual clean(qs)
+      forAll(querystringGen(validParamGen)) { qs =>
+        params(qs).map(toNVP).map(fromNVP).right.value shouldEqual javaDecode(qs).right.value
       }
     }
     "produce BadRow for invalid line data" in {
       orBadRow(null, None) should be('left)
     }
   }
+
+  val mkQS: Map[String, String] => String                    = _.map { case (k, v) => s"$k=$v" }.mkString("&")
+  val javaEncode: String        => Either[Throwable, String] = x => Either.catchNonFatal(URLEncoder.encode(x, UTF_8.toString))
+  val javaDecode: String        => Either[Throwable, String] = x => Either.catchNonFatal(URLDecoder.decode(x, UTF_8.toString))
 }
