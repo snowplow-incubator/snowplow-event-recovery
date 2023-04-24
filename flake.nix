85ccc0d51dc8ad4c4ef7ebac8959e3093297ef07
Piotr Limanowski <plimanowski@pm.me>
2023-04-24 11:54:50 +0200

    Add CPFV handling for double Base64-encoded payloads
    Adds a way to recover collector payload format violations that have been thrown because enrich received an already base64-encoded event. This change will base64-decode and return events to collector payloads stream.

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 {
+
   description = "applications for recovering snowplow bad rows";
 
   inputs = {
@@ -12,8 +13,25 @@
             inherit system;
             config.allowUnfree = true;
             config.allowUnsupportedSystem = true;
-          }; in {
-            devShell = import ./shell.nix { inherit pkgs; };
-          }
+          };
+          spkgs = rec {
+            jre = pkgs.openjdk11;
+            scala = pkgs.scala.override{inherit jre;};
+            sbt = pkgs.sbt.override{inherit jre;};
+            coursier = pkgs.coursier.override{inherit jre;};
+            metals = pkgs.metals.override{inherit coursier jre;};
+          };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with spkgs; [
+            jre
+            metals
+            sbt
+            pkgs.entr
+            pkgs.awscli
+            pkgs.nodePackages.snyk
+          ];
+        };
+      }
     );
 }
